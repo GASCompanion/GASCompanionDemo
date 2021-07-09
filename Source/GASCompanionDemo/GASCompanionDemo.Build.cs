74e9bddc176788b6f443f6e2a2caecf14825ae9b
Mickael Daniel <daniel.mickael@gmail.com>
2021-07-09 23:36:59 +0200

    Update demo project to use new launchpad examples plugin as submodule

---
+++ Source/GASCompanionDemo/GASCompanionDemo.Build.cs
@@ -0,0 +1,21 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+using UnrealBuildTool;
+
+public class GASCompanionDemo : ModuleRules
+{
+	public GASCompanionDemo(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore" });
+
+		PrivateDependencyModuleNames.AddRange(new string[] {
+			"GASCompanion",
+			"GameplayAbilities",
+			"GameplayTasks",
+			"GameplayTags",
+		});
+
+	}
+}
